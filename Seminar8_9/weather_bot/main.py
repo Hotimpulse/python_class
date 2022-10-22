ecd9f04e63cf62659020331284f9a9aafe972a52
Lev <hotimpulse@hotmail.com>
2022-10-22 13:35:15 +0700

    fix: commenting out line 14 in main

--- Seminar8_9/weather_bot/main.py
+++ Seminar8_9/weather_bot/main.py
@@ -11,7 +11,7 @@ disp = updater.dispatcher
 disp.add_handler(telegram.ext.CommandHandler("start", start))
 disp.add_handler(telegram.ext.CommandHandler("help", commands))
 disp.add_handler(telegram.ext.CommandHandler("forecast", get_weather))
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_msg))
+# disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_msg))
 disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, get_weather))
 
 updater.start_polling()
